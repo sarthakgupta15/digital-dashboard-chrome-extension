2a6354ececc9efd7ce509ef61eba23048ba25f4a
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-07-26 00:46:36 +0530

    Relax Tab

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,17 +1,17 @@
+// components/SpotifyPlayer.js
 import React from 'react';
-import './SpotifyPlayer.css'; // Import specific styles for SpotifyPlayer component
+import './SpotifyPlayer.css'; // Create this CSS file for styling
 
 const SpotifyPlayer = ({ embedUrl }) => {
   return (
-    <div className="widget spotify-player-widget">
-      <h2>Spotify Player</h2>
-      <iframe 
+    <div className="spotify-player">
+      <iframe
         src={embedUrl}
-        width="300" 
-        height="380" 
-        frameBorder="0" 
-        allowtransparency="true" 
-        allow="encrypted-media" 
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
         title="Spotify Player"
       ></iframe>
     </div>
--- src/pages/RelaxTab.js
+++ src/pages/RelaxTab.js
@@ -2,14 +2,21 @@ import React from 'react';
 import SpotifyPlayer from '../components/SpotifyPlayer';
 import BookQuote from '../components/BookQuote';
 import FitnessReminder from '../components/FitnessReminder';
+import './RelaxTab.css'; // Make sure to import the CSS
 
 const RelaxTab = () => {
   return (
-    <div>
+    <div className="relax-tab">
       <h2>Relax Tab</h2>
-      <SpotifyPlayer embedUrl="https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M" />
-      <BookQuote />
-      <FitnessReminder />
+      <div className="spotify-container">
+        <SpotifyPlayer embedUrl="https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M" />
+      </div>
+      <div className="book-quote-container">
+        <BookQuote />
+      </div>
+      <div className="fitness-reminder-container">
+        <FitnessReminder />
+      </div>
     </div>
   );
 };
